83ffe22d9bf10e8bcd7e89bc619833ac11d9c9f1
firu11 <firu@firu.dev>
2026-01-14 13:02:03 +0100

    add small doc for core pkg

---
+++ core/doc.go
@@ -0,0 +1,4 @@
+// Api interface is the real core with all the logic, while JsonApi is a wrapper handling all the json marshaling/unmarshaling.
+//
+// Use core.NewApi() and core.NewJsonApi() respectively.
+package core
