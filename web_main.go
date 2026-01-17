ce96f109f5b7c8839b7532e443f2287723985efe
firu11 <firu@firu.dev>
2026-01-17 11:30:22 +0100

    cleanup wasm main

--- web_main.go
+++ web_main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/firu11/git-calendar-core/core"
 )
 
+// This is the starting point which gets called from JS.
 func main() {
 	jsonApi := core.NewJsonApi()
 
@@ -17,42 +18,43 @@ func main() {
 }
 
 func RegisterCallbacks(api core.JsonApi) {
-	// we wrap each method
-	js.Global().Set("CalendarCore", js.ValueOf(map[string]any{
-		"initialize": js.FuncOf(func(this js.Value, args []js.Value) any {
-			// error handling: returns a promise to JS
-			return wrapPromise(func() (any, error) {
-				return nil, api.Initialize()
-			})
+	js.Global().Set("CalendarCore",
+		js.ValueOf(map[string]any{ // we wrap each method
+			"initialize": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return nil, api.Initialize()
+				})
+			}),
+			"addEvent": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return nil, api.AddEvent(args[0].String())
+				})
+			}),
+			"getEvent": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return api.GetEvent(args[0].Int())
+				})
+			}),
+			"getEvents": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return api.GetEvents(int64(args[0].Int()), int64(args[1].Int()))
+				})
+			}),
+			"setCorsProxy": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return nil, api.SetCorsProxy(args[0].String())
+				})
+			}),
+			"clone": js.FuncOf(func(this js.Value, args []js.Value) any {
+				return wrapPromise(func() (any, error) {
+					return nil, api.Clone(args[0].String())
+				})
+			}),
+			// TODO others
 		}),
-		"addEvent": js.FuncOf(func(this js.Value, args []js.Value) any {
-			return wrapPromise(func() (any, error) {
-				return nil, api.AddEvent(args[0].String())
-			})
-		}),
-		"getEvent": js.FuncOf(func(this js.Value, args []js.Value) any {
-			return wrapPromise(func() (any, error) {
-				return api.GetEvent(args[0].Int())
-			})
-		}),
-		"getEvents": js.FuncOf(func(this js.Value, args []js.Value) any {
-			return wrapPromise(func() (any, error) {
-				return api.GetEvents(int64(args[0].Int()), int64(args[1].Int()))
-			})
-		}),
-		"setCorsProxy": js.FuncOf(func(this js.Value, args []js.Value) any {
-			return wrapPromise(func() (any, error) {
-				return nil, api.SetCorsProxy(args[0].String())
-			})
-		}),
-		"clone": js.FuncOf(func(this js.Value, args []js.Value) any {
-			return wrapPromise(func() (any, error) {
-				return nil, api.Clone(args[0].String())
-			})
-		}),
-		// TODO others
-	}))
+	)
 
+	// tell JS we are ready
 	if readyFn := js.Global().Get("onWasmReady"); readyFn.Type() == js.TypeFunction {
 		readyFn.Invoke()
 	}
@@ -66,14 +68,11 @@ func wrapPromise(fn func() (any, error)) any {
 
 		go func() {
 			res, err := fn()
-			if err != nil {
-				// cretes a JS new Error(message) and invokes it (throws it whatever)
-
-				// Check if the error is a wrapper for a JS value
-				// If it is, try to get the 'message' property
+			if err != nil { // create a JS new Error(message) and invoke it ("throw" it or whatever)
+				// get the error text (default behavior)
 				errorMessage := err.Error()
 
-				// This is a trick to see if the error contains a JS value
+				// check if the error is a wrapper for a JS value; if it is, try to get the "message" property and use it instead
 				if jsErr, ok := err.(js.Error); ok {
 					message := jsErr.Value.Get("message")
 					if message.Truthy() {
@@ -81,15 +80,18 @@ func wrapPromise(fn func() (any, error)) any {
 					}
 				}
 
+				// create the JS Error object
 				errorConstructor := js.Global().Get("Error")
 				reject.Invoke(errorConstructor.New(errorMessage))
-			} else {
+			} else { // no error, pass the result
 				resolve.Invoke(js.ValueOf(res))
 			}
 		}()
+
 		return nil
 	})
 
+	// return a JS Promise
 	promiseClass := js.Global().Get("Promise")
 	return promiseClass.New(handler)
 }
