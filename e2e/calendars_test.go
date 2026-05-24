9dceed62e5488cf7d03fe9e893a14db59058d792
firu11 <firu@firu.dev>
2026-05-24 15:21:25 +0200

    more calendar tests

--- e2e/calendars_test.go
+++ e2e/calendars_test.go
@@ -42,3 +42,58 @@ func TestCreateCalendar(t *testing.T) {
 		t.Error("directory not found")
 	}
 }
+
+func TestListCalendars(t *testing.T) {
+	c := core.NewCore()
+
+	err := c.CreateCalendar(TestCalendarName, "")
+	if err != nil {
+		t.Fatalf("failed to create calendar: %v", err)
+	}
+
+	t.Cleanup(func() {
+		_ = c.RemoveCalendar(TestCalendarName)
+	})
+
+	var found bool
+	for _, calendar := range c.ListCalendars() {
+		if calendar == TestCalendarName {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("calendar %q not found in list", TestCalendarName)
+	}
+}
+
+func TestRemoveCalendar(t *testing.T) {
+	c := core.NewCore()
+
+	err := c.CreateCalendar(TestCalendarName, "")
+	if err != nil {
+		t.Fatalf("failed to create calendar: %v", err)
+	}
+
+	err = c.RemoveCalendar(TestCalendarName)
+	if err != nil {
+		t.Fatalf("failed to remove calendar: %v", err)
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		t.Errorf("failed to get home dir: %v", err)
+	}
+
+	calendarPath := filepath.Join(home, filesystem.DirName, TestCalendarName)
+
+	_, err = os.Stat(calendarPath)
+	if err == nil {
+		t.Error("calendar directory still exists")
+	}
+
+	if err != nil && !os.IsNotExist(err) {
+		t.Errorf("failed to check calendar directory: %v", err)
+	}
+}
