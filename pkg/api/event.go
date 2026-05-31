e7b3bce7c264f92eb991e95841fdba92d0061dae
firu11 <firu@firu.dev>
2026-05-31 21:49:12 +0200

    cleanup

--- pkg/api/event.go
+++ pkg/api/event.go
@@ -4,22 +4,22 @@ package api
 //
 // This event isn't used in Go itself, but serves as a "shape definition" for `gomobile` to bind it into Kotlin/Swift.
 type Event struct {
-	Id          string      `json:"id"`
-	Title       string      `json:"title"`
-	Location    string      `json:"location"`
-	Description string      `json:"description"`
-	From        string      `json:"from"` // RFC3339 format e.g., 2009-11-10T23:00:00Z (the default format of json.Marshal() for time.Time)
-	To          string      `json:"to"`   // RFC3339 format e.g., 2009-11-10T23:00:00Z (the default format of json.Marshal() for time.Time)
-	Calendar    string      `json:"calendar"`
-	Tag         string      `json:"tag"`
-	ParentId    string      `json:"parentId"`
-	Repeat      *Repetition `json:"repeat"`
+	Id          string
+	Title       string
+	Location    string
+	Description string
+	From        string // RFC3339 format e.g., 2009-11-10T23:00:00Z (the default format of json.Marshal() for time.Time)
+	To          string // RFC3339 format e.g., 2009-11-10T23:00:00Z (the default format of json.Marshal() for time.Time)
+	Calendar    string
+	Tag         string
+	ParentId    string
+	Repeat      *Repetition
 }
 
 type Repetition struct {
-	Frequency  int      `json:"frequency"`
-	Interval   int      `json:"interval"`
-	Until      string   `json:"until"`
-	Count      int      `json:"count"`
-	Exceptions []string `json:"exceptions"`
+	Frequency  int
+	Interval   int
+	Until      string
+	Count      int
+	Exceptions []string
 }
